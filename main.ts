9242a60ceacc0b1c2899b7dc242ff9db899c8079
jordicollell <78138546+jordicollell@users.noreply.github.com>
2021-03-23 13:31:46 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+let llum = 0
+input.onPinPressed(TouchPin.P0, function () {
+    if (llum == 0) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        llum = 1
+    } else {
+        llum = 0
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
